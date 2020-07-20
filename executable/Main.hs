b997b9321b93303660ba8b1b69780d7204aacdda
Victor Miraldo <v.cacciarimiraldo@gmail.com>
2020-07-20 19:00:44 +0200

    Getting the argument parsers up

--- executable/Main.hs
+++ executable/Main.hs
@@ -1,10 +1,50 @@
 module Main where
 
+import           System.Environment
+import           Options.Applicative
+
 import qualified Data.Text as T
 
-import System.Environment
+import           Text.HSpell.Syntax.Dict
 
-import Text.HSpell.Syntax.Dict
+data Options = Options
+  { optsDicts     :: [FilePath]
+  , optsPrefixLen :: Int
+  , optsEditDist  :: Int
+  } deriving (Eq , Show)
+
+optsParser :: Parser Options
+optsParser = Options
+  <$> optsDictsParser
+  <*> optsPrefixLenParser
+  <*> optsEditDistParser
+
+optsDictsParser :: Parser [FilePath]
+optsDictsParser = option _ $
+     long "dicts"
+  <> short 'd'
+  <> metavar "FILE,FILE,..."
+  <> help "List of dictionaries to load"
+
+optsPrefixLenParser :: Parser Int
+optsPrefixLenParser = option auto $
+     long "prefix-len"
+  <> short 'p'
+  <> showDefault
+  <> value 4
+  <> metavar "INT"
+  <> help "Length of prefix to precompute deletions for."
+
+optsEditDistParser :: Parser Int
+optsEditDistParser = option auto $
+     long "edit-dist"
+  <> short 'e'
+  <> showDefault
+  <> value 2
+  <> metavar "INT"
+  <> help "Maximum Damerau-Leveshtein distance to suggest spelling corrections."
+
+
 
 main :: IO ()
 main = do args <- getArgs
