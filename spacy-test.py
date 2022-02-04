52cdcc2b95a44e4565a5d81b00fad0e6410a674d
Victor Miraldo <v.cacciarimiraldo@gmail.com>
2022-02-04 20:41:13 +0100

    Test using spacy in a repl-like fashion. Works well!

--- spacy-test.py
+++ spacy-test.py
@@ -1,7 +1,22 @@
-import spacy
+import spacy, sys
 
 # Load English tokenizer, tagger, parser and NER
 nlp = spacy.load("en_core_web_sm")
-doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
-for token in doc:
-    print(token.text, token.pos_, token.dep_)
+
+def show_token(tok):
+    if tok.pos_ == 'PUNCT':
+        return '\\' + tok.text
+    else:
+        return tok.text
+
+def analyze(line):
+  doc = nlp(line)
+  for sent in doc.sents:
+      toks = ['{0}:{1}:{2}'.format(show_token(token), token.pos_, token.tag_) for token in sent]
+      [print(t, end=';') for t in toks]
+      print()
+
+if __name__ == '__main__':
+    for line in sys.stdin:
+        analyze(line.rstrip())
+        
